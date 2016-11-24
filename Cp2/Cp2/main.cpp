608272b5c664842cbe7edf7309a69a1c465b64c3
jedoli <jjedoli@naver.com>
2016-11-24 18:44:35 +0900

    ExArray class implement, practice Template

--- Cp2/Cp2/main.cpp
+++ Cp2/Cp2/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <windows.h>
 #include "Frame.h"
+#include "ExArray.h"
 using namespace std;
 class KeyEvent {
 private:
@@ -118,6 +119,13 @@ public:
 
 
 
+	template<typename T>
+	void mySwap(T& num1, T& num2) {
+		T temp = num1;
+		num1 = num2;
+		num2 = temp;
+	}
+
 
 int main() {
 	/*
@@ -132,7 +140,7 @@ int main() {
 	CloseHandle(handleB);
 	*/
 
-
+	/*
 	try {
 		Frame *f1 = new Frame;
 		Frame *f2 = new Frame[10];
@@ -143,6 +151,36 @@ int main() {
 	catch (string msg) {
 		cout << "Error msg : " << msg << endl;
 	}
+	*/
+
+	ExArray<int> arr1(3);
+	ExArray<char> arr2(4);
+	ExArray<double>arr3(5);
+
+	arr1.addData(2);
+	arr2.addData('A');
+	arr3.addData(32.12);
+
+	arr1.printData();
+	arr2.printData();
+	arr3.printData();
+
+	int num1 = 10, num2 = 40;
+	cout << "before : " << num1 << "  " << num2 << endl;
+	mySwap<int>(num1, num2);
+	cout << "after : " << num1 << "  " << num2 << endl;
+
+	double numd1 = 10, numd2 = 40;
+	cout << "before : " << numd1 << "  " << numd2 << endl;
+	mySwap<double>(numd1, numd2);
+	cout << "after : " << numd1 << "  " << numd2 << endl;
+
+	ExArray<int> arr4(5);
+	arr4.addData(10);
+
+	mySwap<ExArray<int>>(arr1, arr4);
+	arr1.printData();
+	arr4.printData();
 
 	return 0;
 }
