16622ea20ebf4b8ea09eebf30a68d713f048c5b1
unknown <jjedoli@naver.com>
2016-12-01 16:44:05 +0900

    STL practice ok

--- Cp2/Cp2/main.cpp
+++ Cp2/Cp2/main.cpp
@@ -1,9 +1,14 @@
 #include <process.h>
 #include <iostream>
-#include <windows.h>
-#include "Frame.h"
-#include "ExArray.h"
+//#include <windows.h>
+//#include "Frame.h"
+//#include "ExArray.h"
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
 using namespace std;
+/*
 class KeyEvent {
 private:
 	HANDLE hln;
@@ -33,7 +38,7 @@ public:
 	}
 
 };
-
+*/
 /*
 unsigned int __stdcall mythread(void*) {
 	int i = 0;
@@ -59,7 +64,7 @@ unsigned int __stdcall keyEvent(void*) {
 	return 0;
 }
 */
-
+/*
 class Game {
 private:
 	int x;
@@ -126,6 +131,27 @@ public:
 		num2 = temp;
 	}
 
+*/
+
+void dump(list<string> &l) {
+	list<string>::iterator iter = l.begin();
+	while (iter != l.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+}
+
+class Word {
+private:
+	string word;
+public:
+	Word(string word) {
+		this->word = word;
+	}
+	string getWord() {
+		return this->word;
+	}
+};
 
 int main() {
 	/*
@@ -153,6 +179,7 @@ int main() {
 	}
 	*/
 
+	/*
 	ExArray<int> arr1(3);
 	ExArray<char> arr2(4);
 	ExArray<double>arr3(5);
@@ -181,6 +208,89 @@ int main() {
 	mySwap<ExArray<int>>(arr1, arr4);
 	arr1.printData();
 	arr4.printData();
+	*/
+/*
+	vector<int> v;
+	v.push_back(10);
+	v.push_back(20);
+	v.push_back(30);
+	v.insert(v.begin(), 40);
+	v.insert(v.end(), 50);
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+
+	cout << "access index at 1: " << v.at(1) << endl;
+
+	v.erase(v.begin());
+	v.erase(v.end() - 1);
+	vector<int>::iterator iter = v.begin();
+
+	cout << "access index at 1 with iterator: " << iter[1] << endl;
+
+	
+	while (iter != v.end()) {
+		cout << *iter << endl;
+		iter++;
+	}
+	*/
+	/*
+	list<string> names;
+	names.insert(names.begin(), "Konkuk");
+	names.insert(names.end(), "University");
+	names.insert(names.end(), "SCLAB");
+	names.insert(names.end(), "CSY");
+	dump(names);
+	cout << "===" << endl;
+	names.reverse();
+	dump(names);
+	*/
+	/*
+	map<string, int> m;
+	m["seoul"] = 100;
+	m["daegu"] = 120;
+	m["busan"] = 200;
+
+	cout << "Train to Seoul = " << m["seoul"] << "$" << endl;
+	cout << "Train to Daegu = " << m["daegu"] << "$" << endl;
+	cout << "Train to Busan = " << m["busan"] << "$" << endl;
+	*/
+
+	map<char, list<Word>>m;
+	list<Word> aList;
+	list<Word> bList;
+
+	aList.push_back(Word("apple"));
+	aList.push_back(Word("avoid"));
+	aList.push_back(Word("appear"));
+
+	bList.push_back(Word("bread"));
+	bList.push_back(Word("bring"));
+	bList.push_back(Word("bow"));
+
+	m['a'] = aList;
+	m['b'] = bList;
+
+	map<char, list<Word>>::const_iterator iter = m.begin();
+		
+	while (iter != m.end()) {
+		cout << iter->first << ":" << endl;
+		list<Word> temp = iter->second;
+		list<Word>::const_iterator iter_list = temp.begin();
+
+		while (iter_list != temp.end()) {
+			Word word = *iter_list;
+			cout << word.getWord() << ", ";
+			iter_list++;
+		}
+
+		cout << "\n===" << endl;
+		iter++;
+	}
+
+
+
 
 	return 0;
 }
